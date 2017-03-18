6aac1b802f007997f97328af3ea7c0a379dad56a
Mahdi H. Osman <bokore@gmail.com>
2017-03-18 00:29:54 -0700

    workshop schedule changes

--- workshops/js/dataTimeline.js
+++ workshops/js/dataTimeline.js
@@ -5,18 +5,17 @@ timelineEvents['0' + (today.getMonth() + 1) + '-' + today.getDate() + '-' + toda
         {content: 'Room 2 - “Introduction to Coding - Gotta Code ‘Em All” by Make School ', startTime: '01:00', endTime: '01:30'},
         {content: 'Room 1 - “Introduction to Android Development” by ACM ', startTime: '01:30', endTime: '02:00'},
         {content: 'Room 2 - “How to Hack With Git” by Tobiah Zarlez, Microsoft', startTime: '02:00', endTime: '02:30'},
-        {content: 'Room 2 - “Intermediate iOS: Build Your First Mobile App” by Make School', startTime: '03:00', endTime: '03:30'},
-        {content: 'Room 2 - “Building a Successful App Business with Analytics” by Toby Vogelz at Flurry Analytics', startTime: '04:00', endTime: '04:30'},
-        {content: 'Room 1 - “Autolayout: Saving Time and Hassle The Hard Way” by Ty Daniels at Grain', startTime: '04:30', endTime: '05:00'},
-        {content: 'Room 2 - TBD by Alex Cory at Table.co', startTime: '04:30', endTime: '05:00'},
-				{content: 'Room 1 - “Design, but for Humans” by Elaine Chow at Lingrove ', startTime: '05:00', endTime: '05:30'},
+				{content: 'Room 1 - “Intermediate iOS: Build Your First Mobile App” by Make School', startTime: '02:30', endTime: '03:00'},
+				{content: 'Room 2 - “Intro to React.js” by Alex Cory at Table.co', startTime: '03:00', endTime: '03:30'},
+				{content: 'Room 1 - “Building a Successful App Business with Analytics” by Toby Vogelz at Flurry Analytics', startTime: '03:30', endTime: '04:00'},
+        {content: 'Room 2 - “Autolayout: Saving Time and Hassle The Hard Way” by Ty Daniels at Grain', startTime: '04:00', endTime: '04:30'},
+        //{content: 'Room 2 - TBD by Alex Cory at Table.co', startTime: '04:30', endTime: '05:00'},
+				{content: 'Room 1 - “Design, but for Humans” by Elaine Chow at Lingrove ', startTime: '05:30', endTime: '06:00'},
         {content: 'Room 2 - “Buy Low Sell High: The Science Behind Bitcoin Trading” by Stefan Quin', startTime: '07:00', endTime: '07:30'},
         {content: 'Room 1 - “Cybersecurity: No Zero Day? No Problem, I’ll Just Ask for Access” by Josh Cohen at Cosmo’s Consulting', startTime: '07:30', endTime: '08:00'},
         {content: 'Room 2 - “How to Build an Automated Events Marketing Machine with Zero Employees” by Josh Fetcher at AutoPilot', startTime: '08:00', endTime: '08:30'},
         {content: 'Room 1 - “How to Pitch Your Idea/Startup” by Draper University', startTime: '08:30', endTime: '09:00'},
-        {content: 'Innovation Lounge - Cup Stacking Tournament by Major League Hacking', startTime: '09:00', endTime: '10:00'},
-        {content: 'Room 2 - “New to San Francisco? How to Explore Start-up HQs” by INCUBED', startTime: '11:00', endTime: '11:30'},
-
-
+        {content: 'Innovation Lounge - Cup Stacking Tournament by Major League Hacking', startTime: '09:00', endTime: '10:30'},
+        {content: 'Room 1 - “New to San Francisco? How to Explore Start-up HQs” by INCUBED', startTime: '10:30', endTime: '11:00'},
 
 	];
